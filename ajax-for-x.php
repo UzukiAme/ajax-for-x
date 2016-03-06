e7da82853ff9ef13aa4a7a605b1480337dbfdd59
April <volite06@hotmail.com>
2016-03-06 12:25:47 -0500

    modify plugin descripton

--- ajax-for-x.php
+++ ajax-for-x.php
@@ -1,6 +1,6 @@
 /*
 Plugin Name: Ajax For X
-Description: Adds AJAX functionality to X theme via Angular.
+Description: Adds AJAX functionality and page animations to X theme via Angular, JQuery, and GSAP to give the feel of a one page application
 Author: April Carter
 Version: 1.0
 License: GPL2
