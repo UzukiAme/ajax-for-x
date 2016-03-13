6d4ab78ccb7b4ff13b7f89dc65de92a591c18fa3
April <april@decographic.net>
2016-03-13 14:42:40 -0400

    Use jQuery AJAX to get an manipulate html on the client side rather than trying to use $routeProvider and ng-view. May use directives to insert content.
    Determined that there was no easy way to use $routeProvider with urls
    outside the app root because $route compares current url to
    $location.path(), which returns an empty string if the location is outside
    the app root.

--- ajax-for-x.php
+++ ajax-for-x.php
@@ -1,34 +1,16 @@
 <?php
 /*
 Plugin Name: Ajax For X
-Description: Adds AJAX functionality and page animations to X theme via Angular, JQuery, and GSAP to create the feel of a one page application
-Author: April Carter
-Version: 1.0
-License: GPL2
+Plugin Description: Give existing WP content the feel of a one page application
 */
 
-add_action('wp_enqueue_scripts', 'afx_resources_js');
-function afx_resources_js() {
-	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js');
-	wp_enqueue_script('angular', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular.min.js');
-	wp_enqueue_script('angular-route', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular-route.min.js');
-	wp_enqueue_script('angular-sanitize', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular-sanitize.min.js');
-	wp_enqueue_script('main', plugins_url('main.js', __FILE__), array(), false, true);
-	wp_enqueue_script('ajax-for-x-js', plugins_url('ajax-for-x.js', __FILE__), array(), false, true);
-}
+add_action('wp_enqueue_scripts', 'ajax_for_x_scripts');
 
-add_action('x_before_site_begin', 'init_main_page_angular');
-add_action('x_after_site_end', 'close_main_page_angular');
-function init_main_page_angular() {
-	echo "<div ng-app='toOnePageApp' class='top-level' controller='mainCtrl'>";
-}
-function close_main_page_angular() {
-	echo "</div>";
-}
-
-add_action('the_content', insert_content);
-function insert_content($content) {
-	return $content;
+function ajax_for_x_scripts() {
+  wp_enqueue_script('angular','https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js');
+  wp_enqueue_script('angular-route', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-route.js');
+  wp_enqueue_script('main-js', plugins_url() . "/ajax-for-x/main.js", array(), false, true);
+  wp_enqueue_style('main-css', plugins_url() . "/ajax-for-x/assets/main.css");
 }
 
 ?>
--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 (function($) {
-	var baseUrl = "localhost/newdeco/";
+  var html;
+  //This will contain routing logic which uses regex to avoid dozens of explicit definitions
+  //will eventually be in a function
+  //Custom events and "stopPropigation()" will likely be used to target specific page requests.
+  $(".x-btn").click(function() {
+    html = $.get("ajax-toy");
+  });
+
+  //success split off in anticipation of differing formatting and success needs depending on page requested.
+  function format_response(ajaxResponse) {
+    return $(ajaxResponse.responseText).find(".content").wrapInner("<div class='hidden'>").html();
+  }
+  //insertion logic must take into account the possibility of a menu click vs a btn click
+  $(document).on("ajaxSuccess", function() {
+    html = format_response(html);
+    console.log(html);
+  });
 })(jQuery)
