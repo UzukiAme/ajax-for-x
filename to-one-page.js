1464d73c421cf2b6905a256839cb5d15ebb13bb0
April <april@decographic.net>
2016-03-26 20:20:31 -0400

    branch change

--- to-one-page.js
+++ to-one-page.js
@@ -1,1 +1,54 @@
-console.log(jQuery(".to-one-pg-btn"));
+(function($) {
+  //Static elements
+  $(".content").append("<div class='top-pages-container'></div>");
+  $(".top-pages-container").css({
+    marginRight: function() {
+      margin = $(window).height() + 100;
+      return "-" + margin + "px";
+    },
+    width: function() {
+      return $(window).width();
+    }
+  });
+
+  //global variables
+  var globals = {
+    baseUrl: window.location.protocol + "//" + window.location.hostname + "/newdeco",
+    pageContainer: $(".top-pages-container")
+  }
+
+
+/**
+* @description Makes an ajax request for new page content and inserts it into .top-page-container.
+*
+* @param {string} url The url from the element that was clicked, which is used to construct the api endpoint url
+*/
+  function getContent(url) {
+    //create the url to which the content request will be sent
+    var slugPattern = /\/([^/]*)\/?$/,
+      slug = url.match(slugPattern)[1],
+      endPoint = globals.baseUrl + '/wp-json/wp/v2/pages/?slug=' + slug;
+
+    //perform the ajax request
+    $.get(endPoint, function(page) {
+      var contentClass = 'top-' + slug,
+        content = page[0].content.rendered,
+        container = globals.pageContainer;
+      container.trigger("newpage", [contentClass]);
+      //on success, add the content to the page
+      if($("." + contentClass).length == 0) {
+        $(".top-pages-container").append("<div class='" + contentClass + "'></div>");
+        $("." + contentClass).css({
+          display: "none"
+        })
+        .html(content);
+      }
+    });
+  }
+  $(".to-one-pg-btn").click(function(event) {
+    event.preventDefault();
+    var url = event.target.attributes.href.value;
+    getContent(url);
+  });
+
+})(jQuery)
--- to-one-page.php
+++ to-one-page.php
@@ -5,9 +5,16 @@
 */
 
 function to_one_page_scripts() {
-  wp_enqueue_script('top-js', plugins_url() + "/to-one-page/to-one-page.js", array(), false, true);
+  wp_enqueue_script('gsap', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.2/TweenMax.min.js');
+  wp_enqueue_script('top-js', plugins_url() . "/to-one-page/to-one-page.js", array(), false, true);
+  wp_enqueue_script('animate-js', plugins_url() . "/to-one-page/assets/animate.js", array(), false, true);
+  wp_enqueue_style('main-css', plugins_url() . "/to-one-page/assets/main.css");
 }
-
 add_action('wp_enqueue_scripts', 'to_one_page_scripts');
+
+function transform_content() {
+
+}
+add_action('the_content');
 
 ?>
