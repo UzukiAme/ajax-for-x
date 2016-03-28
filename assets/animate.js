e7675d739ea81978211322a183e2a8ef8a94971a
April <april@decographic.net>
2016-03-28 00:16:56 -0400

    Create animateOut and set up blank slate for animateIn

--- assets/animate.js
+++ assets/animate.js
@@ -1,18 +1,24 @@
 (function($) {
-  $.fn.reverse = [].reverse;
-
   var visibile = $(".currently-visible");
-  var jBtns = $("to1pg-btn");
-  var btnsObj = document.getElementsByClassName("to1pg-btn");
-
+  
+  /**
+  * @description Remove visibility class
+  */
+  function makeInvisible() {
+      $(".currently-visible.content").toggleClass("currently-visible");
+  }
+    
   function animateOut(url) {
-    var pageOut = new TimelineMax();
-    pageOut.staggerTo($(".to1pg-btn").reverse(), 1.3, {y: $(window).height(), ease: Back.easeIn.config(1.2) }, 0.2)
-    .to();
+    var pageOut = new TimelineMax({onComplete:makeInvisible});
+    pageOut.staggerTo($(".currently-visible .to1pg-btn").reverse(), 0.7, {y: $(window).height(), ease: Back.easeIn.config(1.2) }, 0.2, "start")
+    .to($(".pg-title"), 0.7, {y: -$(window).height(), ease:Back.easeIn.config(1.2)}, "start")
+    .to($(".x-section"), 1, {opacity:0, ease:Circ.easeOut}, "-=0.5")
+    .set($(".currently-visible"), {y: $(window).height(), x: $(window).width()})
+    .set([$(".pg-title"), $(".currently-visible .to1pg-btn"), $(".x-section")], {clearProps:"all"});
   }
   $(".to1pg-btn").click(function(event) {
     var url = event.target.attributes.href.value;
-    animateOut(url);
+    animateOut();
   });
   // /**
   // * @description Toggle "currently-visible" class
@@ -23,7 +29,9 @@
   // $("body").on('outFin', function(event, targets) {
   //   visibility(targets.from, targets.to);
   // });
-})(jQuery)
+})(jQuery);
+
+
 
 /*Set the containers to absolute
-  they must be relative to the browser window so their visible positions will be 0,0
+  they must be relative to the browser window so their visible positions will be 0,0*/
--- to-one-page.js
+++ to-one-page.js
@@ -1,53 +1,72 @@
-(function($) {
-  //Static elements
-  $(".content").append("<div class='to1pg-pages-container'></div>").addClass("currently-visible to1pg-home");
-  //set up position of container for incoming and out of rotation content
-  $(".to1pg-pages-container").css({
-    marginRight: function() {
-      margin = $(window).height() + 100;
-      return "-" + margin + "px";
+$(".content").append("<div class='to1pg-container'></div>");
+//global variables
+var globals = {
+    baseUrl: window.location.protocol + "//" + window.location.hostname,
+    currentPage: {
+        pageClass: "to1pg-home",
+        pageExists: true
     },
-    width: function() {
-      return $(window).width();
+    container: $(".to1pg-container")
+  };
+container.css({
+    marginRight: function() {
+        return "-" + $(window).width() + 100 + "px";
     }
-  });
-
-  //global variables
-  var globals = {
-    baseUrl: window.location.protocol + "//" + window.location.hostname + "/newdeco",
-    pageContainer: $(".to1pg-pages-container")
-  }
+});
 
 
 /**
+* @description Gets the slug from a given url
+* @param {string} url The url
+* @returns {string} The slug
+*/
+function getSlug(url) {
+    var slugPattern = /[/#]([^/?]*)\/?$/;
+    return url.match(slugPattern)[1];
+}
+
+//Add ability to reverse JQuery collections.
+jQuery.fn.reverse = [].reverse;
+
+(function($) {
+  //Static elements
+  $(".content").addClass("currently-visible to1pg-home");
+
+/**
 * @description Makes an ajax request for new page content and inserts it into .to1pg-page-container.
-*
 * @param {string} url The url from the element that was clicked, which is used to construct the api endpoint url
 */
   function getContent(url) {
     //create the url to which the content request will be sent
-    var slugPattern = /\/([^/]*)\/?$/,
-      slug = url.match(slugPattern)[1],
-      endPoint = globals.baseUrl + '/wp-json/wp/v2/pages/?slug=' + slug;
+    slug = getSlug(url);
+    var endPoint = globals.baseUrl + '/wp-json/wp/v2/pages/?slug=' + slug;
 
     //perform the ajax request
     $.get(endPoint, function(page) {
       var contentClass = 'to1pg-' + slug,
-        content = page[0].content.rendered,
-        container = globals.pageContainer;
-      container.trigger("newpage", [contentClass]);
+        content = page[0].content.rendered;
       //on success, add the content to the page
-      if($("." + contentClass).length == 0) {
-        $(".to1pg-pages-container").append("<div class='" + contentClass + " hidden'></div>");
-        $("." + contentClass).html(content);
+      if($("." + contentClass).length === 0) {
+        globals.currentPage.pageClass = contentClass;
+        container.append("<div class='" + contentClass + " single-pg'></div>");
+        //css hides new content automatically. Animation code toggles currently-visible class for animation in
+        $("." + contentClass).css({
+            left: function() {
+                return $(window).width() + 100 + "px";
+            }
+        }).html(content);
       }
+    })
+    .fail(function() {
+        globals.currentPage.pageExists = false;
     });
   }
+  
   $(".to1pg-btn").click(function(event) {
     event.preventDefault();
-    $(event.target).trigger("secondClick");
     var url = event.target.attributes.href.value;
     getContent(url);
   });
 
-})(jQuery)
+})(jQuery);
+
