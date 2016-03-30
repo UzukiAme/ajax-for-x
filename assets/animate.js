da430ed5535eda1f495cb6b45f43c0d66a9f6c62
April <april@decographic.net>
2016-03-30 18:43:16 -0400

    Set up animation for error page and add error handling to ajax request for pages

--- assets/animate.js
+++ assets/animate.js
@@ -2,33 +2,89 @@
   var visibile = $(".currently-visible");
   
   /**
-  * @description Remove visibility class
+  * @description Animation sequence for reveal of new content
   */
-  function makeInvisible() {
-      $(".currently-visible.content").toggleClass("currently-visible");
+  function pageIn() {
+    var page = globals.currentPage.pageClass;
+    var enter = new TimelineMax();
+    enter.set($("." + page + " .to1pg-btn"), {x: $(window).width()});
+    $("." + page).toggleClass("currently-visible");
+    enter.from($("." + page), 1.3, {opacity: 0}, "start")
+    .staggerTo($("." + page + " .to1pg-btn"), 0.7, {x: 0, ease: Back.easeOut.config(2.5)}, 0.3, "start")
+    .from($(".pg-title"), 1, {yPercent: -300, ease: Bounce.easeOut});
   }
-    
-  function animateOut(url) {
-    var pageOut = new TimelineMax({onComplete:makeInvisible});
-    pageOut.staggerTo($(".currently-visible .to1pg-btn").reverse(), 0.7, {y: $(window).height(), ease: Back.easeIn.config(1.2) }, 0.2, "start")
-    .to($(".pg-title"), 0.7, {y: -$(window).height(), ease:Back.easeIn.config(1.2)}, "start")
+  
+  /**
+  * @description Remove the visibility class.
+  */
+  function removeVisibility() {
+      $(".currently-visible").removeClass("currently-visible");
+      $(".content").trigger("outfin");
+  }
+      
+  /**
+  * @description Animation sequence for content exit. On completion, the in animation is executed.
+  */ 
+  function pageOut() {
+    var exit = new TimelineMax({onComplete:removeVisibility});
+    exit.staggerTo($(".currently-visible .to1pg-btn").reverse(), 0.7, {y: $(window).height(), ease: Back.easeIn.config(1.2) }, 0.2, "start")
+    .to($(".pg-title"), 0.7, {y: -$(window).height(), ease:Back.easeIn.config(1)}, "start")
     .to($(".x-section"), 1, {opacity:0, ease:Circ.easeOut}, "-=0.5")
-    .set($(".currently-visible"), {y: $(window).height(), x: $(window).width()})
-    .set([$(".pg-title"), $(".currently-visible .to1pg-btn"), $(".x-section")], {clearProps:"all"});
+    .set($(".currently-visible"), {x: $(window).width(), clearProps: "opacity"})
+    .set([$(".pg-title"), $(".currently-visible .to1pg-btn")], {clearProps:"all"});
   }
+  
   $(".to1pg-btn").click(function(event) {
-    var url = event.target.attributes.href.value;
-    animateOut();
+    pageOut();
   });
-  // /**
-  // * @description Toggle "currently-visible" class
-  // */
-  // function visibility(removeFrom, addTo) {
-  //   $(removeFrom, addTo).toggleClass();
-  // }
-  // $("body").on('outFin', function(event, targets) {
-  //   visibility(targets.from, targets.to);
-  // });
+  
+  /**
+  * @description Animate in an error page or message that is dynamically appended to the page depending on whether or not the request for the error page failed
+  */
+  function errorIn() {
+        if($(".to1pg-error").length === 0) {
+            $(".page").append("<div class='to1pg-inline-error single-pg'>We're having a hard time connecting. Try refreshing the page.</div>");
+            var inlineError = new TimelineMax();
+            inlineError.set($(".to1pg-inline-error"), {left: 0, top: 0, opacity: 0, scale:.8});
+            $(".to1pg-inline-error").addClass("currently-visible");
+            inlineError.to($(".to1pg-inline-error"), .5, {opacity: 1, scale: 1});
+        } else {
+            var error = new TimelineMax();
+            error.set($(".to1pg-error"), {left: 0, top: 90, scale:.9, opacity: 0});
+            $(".to1pg-error").addClass("currently-visible");
+            error.to($(".error-title"), .3, {opacity: 1})
+            .to($(".to1pg-error"), .5, {opacity: 1, scale: 1, ease: Back.easeOut.config(1.5)});
+        }  
+    }
+  
+  /**
+  * @description Animate in the error page if it exists, or append an error message div to the body and animate that in
+  */
+  function pageOrError() {
+      var exists = globals.currentPage.exists,
+        done = globals.currentPage.done,
+        pClass = globals.currentPage.pageClass,
+        slug = globals.currentPage.reqSlug;
+    
+    if(exists && done && pClass == ("to1pg-" + slug)) {
+        pageIn();
+    } else if(!done) {
+        $("body").on("ajaxComplete", function() {
+            //stop and hide loading animation before 
+            if(globals.currentPage.exists) {
+                pageIn();    
+            } else {
+                errorIn();
+            }
+        }); 
+    } else if(done && !exists) {
+        errorIn();
+    }
+  }
+  
+  $("body").on("outfin", function(event) {
+    pageOrError();
+  });
 })(jQuery);
 
 
--- to-one-page.js
+++ to-one-page.js
@@ -1,18 +1,20 @@
-$(".content").append("<div class='to1pg-container'></div>");
+//static elements
+jQuery(".page").append("<div class='to1pg-container'></div>");
+
 //global variables
 var globals = {
     baseUrl: window.location.protocol + "//" + window.location.hostname,
     currentPage: {
         pageClass: "to1pg-home",
-        pageExists: true
+        exists: true,
+        done: true,
+        reqSlug: ""
     },
-    container: $(".to1pg-container")
+    container: jQuery(".to1pg-container")
   };
-container.css({
-    marginRight: function() {
-        return "-" + $(window).width() + 100 + "px";
-    }
-});
+globals.container.css({
+    marginRight: -(jQuery(window).width() + 100)
+    });
 
 
 /**
@@ -22,44 +24,68 @@ container.css({
 */
 function getSlug(url) {
     var slugPattern = /[/#]([^/?]*)\/?$/;
-    return url.match(slugPattern)[1];
+    var slug = url.match(slugPattern)[1];
+    globals.currentPage.reqSlug = slug;
+    return slug;
 }
 
 //Add ability to reverse JQuery collections.
 jQuery.fn.reverse = [].reverse;
 
 (function($) {
-  //Static elements
-  $(".content").addClass("currently-visible to1pg-home");
-
+  $(".content").addClass("currently-visible to1pg-home single-pg");
+  
 /**
-* @description Makes an ajax request for new page content and inserts it into .to1pg-page-container.
+* @description Makes an ajax request for new page content and inserts it into .to1pg-container. Update the globals object with the class of the page animate in
+*     and ajax failure indicator (globals.currentPage.pageExists), if necesarry.
 * @param {string} url The url from the element that was clicked, which is used to construct the api endpoint url
 */
   function getContent(url) {
+    globals.currentPage.done = false;
+    function setDone() {
+        globals.currentPage.done = true;  
+        $(".page").off('ajaxComplete', setDone);
+    }
     //create the url to which the content request will be sent
-    slug = getSlug(url);
-    var endPoint = globals.baseUrl + '/wp-json/wp/v2/pages/?slug=' + slug;
-
-    //perform the ajax request
-    $.get(endPoint, function(page) {
-      var contentClass = 'to1pg-' + slug,
-        content = page[0].content.rendered;
-      //on success, add the content to the page
-      if($("." + contentClass).length === 0) {
-        globals.currentPage.pageClass = contentClass;
-        container.append("<div class='" + contentClass + " single-pg'></div>");
-        //css hides new content automatically. Animation code toggles currently-visible class for animation in
-        $("." + contentClass).css({
-            left: function() {
-                return $(window).width() + 100 + "px";
+    var slug = getSlug(url),
+      endPoint = globals.baseUrl + '/wp-json/wp/v2/pages/?slug=' + slug, 
+      contentClass = 'to1pg-' + slug;
+      globals.currentPage.pageClass = contentClass;
+      
+    //perform ajax request if request hasn't already been made before
+    if($("." + contentClass).length === 0) {
+        var promise = $.get(endPoint, function(page) {
+            if(page[0] === undefined) {
+                slug = "error";
+                endPoint = globals.baseUrl + "/wp-json/wp/v2/pages/?slug=" + slug;
+                if($(".to1pg-error").length === 0) {
+                    $.get(endPoint, function(ePage) {
+                        if(ePage[0] === undefined) {
+                            $(".content").trigger("errorFail");
+                            globals.currentPage.exists = false;
+                        } else {
+                            var content = ePage[0].content.rendered;
+                            $(".page").append("<div class='to1pg-error single-pg'>" + content + "</div>");
+                            $(".to1pg-error").css("position", "absolute");
+                            globals.currentPage.pageClass = "to1pg-error";
+                        }
+                    });
+                }
+                globals.currentPage.exists = false;
+            } else {
+                var content = page[0].content.rendered;
+                //on success, add the content to the page
+                globals.container.append("<div class='" + contentClass + " single-pg'></div>");
+                //css hides new content automatically. Animation code toggles currently-visible class for animation in
+                $("." + contentClass).css({
+                    position: "absolute"
+                }).html(content);
             }
-        }).html(content);
-      }
-    })
-    .fail(function() {
-        globals.currentPage.pageExists = false;
-    });
+        });
+    } else {
+        $(".content").trigger("newPage");
+    }
+    $(".page").on("ajaxComplete", setDone);
   }
   
   $(".to1pg-btn").click(function(event) {
@@ -67,6 +93,5 @@ jQuery.fn.reverse = [].reverse;
     var url = event.target.attributes.href.value;
     getContent(url);
   });
-
 })(jQuery);
 
